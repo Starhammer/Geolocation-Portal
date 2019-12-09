ce6bc26395d864199f14a42215e2c752f97ad111
Micha <michael.kress92@web.de>
2019-12-09 11:39:00 +0100

    Bootstrap Version aktualisiert; Layout bearbeitet; Login Maske hinzugefügt; Impressum fertiggestellt; LoginModel erstellt; Font Awesome Icons eingebunden

--- Geolocation-Portal_Test/App_Start/BundleConfig.cs
+++ Geolocation-Portal_Test/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace Geolocation_Portal_Test
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Contenct/font-awesome.min.css"));
         }
     }
 }
---
+++ Geolocation-Portal_Test/Models/LoginModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Geolocation_Portal_Test.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage ="Gib bitte deinen Benutzernamen ein.")]
+        [Display(Name ="Benutzername:")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage ="Gib bitte dein Passwort ein.")]
+        [Display(Name ="Passwort:")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name ="Angemeldet bleiben:")]
+        public bool RememberMe { get; set; }
+    }
+}
