31a5419bc9a7fb4b51d9dd6a5e78e17c1dbe81c7
Micha <michael.kress92@web.de>
2019-12-11 01:09:56 +0100

    Stylesheet bearbeitet; Kontaktseite bearbeitet (Contact Model hinzugefügt) -> Derzeit noch fehlerhaft; Impressum, Nutzungsbedingungen fertiggestellt; SuccessMessage Methode für Kontaktformular erstellt; Layout bearbeitet; Barrierefreiheit implementiert (Session Variablen eingefügt)

--- Geolocation-Portal_Test/Controllers/HomeController.cs
+++ Geolocation-Portal_Test/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Geolocation_Portal_Test.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -33,6 +36,33 @@ namespace Geolocation_Portal_Test.Controllers
 
             return View();
         }
+
+        /* Auskommentiert von Michael, weil der Code aktuell noch nicht einwandfrei funktioniert (Kontaktformular)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Kontakt(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var mail = new MailMessage();
+                mail.To.Add(new MailAddress(model.SenderEmail));
+                mail.Subject = "Your Email Subject";
+                mail.Body = string.Format("<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>", model.SenderName, model.SenderEmail, model.Message);
+                mail.IsBodyHtml = true;
+                using (var smtp = new SmtpClient())
+                {
+                    await smtp.SendMailAsync(mail);
+                    return RedirectToAction("SuccessMessage");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult SuccessMessage()
+        {
+            return View();
+        }
+        */
 
         public ActionResult Nutzungsbedingungen()
         {
