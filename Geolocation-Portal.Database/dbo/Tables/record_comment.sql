82b17355fcc169be9276737e75535a8998219bcd
Starhammer <manuelscheub@gmail.com>
2019-12-12 22:14:59 +0100

    Datenbankschema aktualisiert

---
+++ Geolocation-Portal.Database/dbo/Tables/record_comment.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[record_comment]
+(
+	[dataset_id] INT NOT NULL PRIMARY KEY, 
+    [comment_id] INT NOT NULL
+)
---
+++ Geolocation-Portal.Database/dbo/Tables/role.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[role]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [name] NVARCHAR(50) NOT NULL, 
+    [description] NTEXT NULL
+)
