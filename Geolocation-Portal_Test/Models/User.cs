cd24ac03968e41131c8b26ae1206829f312ceb87
Starhammer <manuelscheub@gmail.com>
2019-12-12 22:53:10 +0100

    Getter und Setter für User testklasse

--- Geolocation-Portal_Test/Models/User.cs
+++ Geolocation-Portal_Test/Models/User.cs
@@ -5,7 +5,18 @@ namespace Geolocation_Portal_Test.Models
 {
     public class User
     {
-        
+        public int id { get; set; }
+        public int role_id { get; set; }
+        public int department_id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public DateTime last_password_change { get; set; }
+        public DateTime create_date { get; set; }
+        public bool account_active { get; set; }
+        public int login_attempts { get; set; }
+        public DateTime last_login { get; set; }
     }
 
     public class UserDBContext : DbContext
