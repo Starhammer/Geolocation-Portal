182261b8fa6610b4ab1c162a4d4e2af19d671654
Starhammer <manuelscheub@gmail.com>
2019-12-12 22:46:34 +0100

    Default Connectionstring hinzugefügt. Testweise auch einen für User angelegt. Test noch ausstehend

---
+++ Geolocation-Portal_Test/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Geolocation_Portal_Test.Models
+{
+    public class User
+    {
+        
+    }
+
+    public class UserDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
