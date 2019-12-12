0577e0616fa7e9b79d0c765d6b64c5f2667a153e
Starhammer <manuelscheub@gmail.com>
2019-12-12 23:23:46 +0100

    Model-Klassen für User und Role imlementiert. test noch ausstehend

---
+++ Geolocation-Portal_Test/Models/Role.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Geolocation_Portal_Test.Models
+{
+    public class Role
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+    }
+
+    public class RoleDBContext : DbContext
+    {
+        public DbSet<Role> Users { get; set; }
+    }
+}
