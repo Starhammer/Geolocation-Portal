4560c632bbebf4509a656787273687b6d2eda79e
Micha <michael.kress92@web.de>
2020-01-12 22:38:39 +0100

    Kontaktformular erstellt und Logik implementiert -> Jedoch nicht getestet (Nicht Bestandteil); OpenData -> Index bearbeitet -> Maximaler Beschreibungstext.

--- Geolocation-Portal_Test/Controllers/HomeController.cs
+++ Geolocation-Portal_Test/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
 using System.Web;
@@ -56,32 +57,46 @@ namespace Geolocation_Portal_Test.Controllers
             return View();
         }
 
-        /* Auskommentiert von Michael, weil der Code aktuell noch nicht einwandfrei funktioniert (Kontaktformular)
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Kontakt(ContactModel model)
+        public ActionResult Kontakt(string name, string senderemail, string message)
         {
-            if (ModelState.IsValid)
-            {
-                var mail = new MailMessage();
-                mail.To.Add(new MailAddress(model.SenderEmail));
-                mail.Subject = "Your Email Subject";
-                mail.Body = string.Format("<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>", model.SenderName, model.SenderEmail, model.Message);
-                mail.IsBodyHtml = true;
-                using (var smtp = new SmtpClient())
-                {
-                    await smtp.SendMailAsync(mail);
-                    return RedirectToAction("SuccessMessage");
-                }
-            }
-            return View(model);
+            SendContactEmail(name, senderemail, message);
+            return Content("Thanks for contacting us! We'll be in contact with you soon!");
         }
 
-        public ActionResult SuccessMessage()
+        /**
+         * https://forums.asp.net/t/2115115.aspx?How+to+setup+a+contact+us+form+
+         */
+        public void SendContactEmail(string user, string senderemail, string message)
         {
-            return View();
+            // Your hard-coded email values (where the email will be sent from), this could be
+            // define in a config file, etc.
+            var email = "{your-email@domain.com}";
+            var password = "{your-password}";
+
+            // Your target (you may want to ensure that you have a property within your form so that you know
+            // who to send the email to
+            string address = "{contact@domain.com}";
+
+            // Builds a message and necessary credentials (example using Gmail)
+            var loginInfo = new NetworkCredential(email, password);
+            var msg = new MailMessage();
+            var smtpClient = new SmtpClient("smtp.gmail.com", 587);
+
+            // This email will be sent from you
+            msg.From = new MailAddress(email);
+            // Your target email address
+            msg.To.Add(new MailAddress(address));
+            msg.Subject = $"{user} just filled out the Contact Form!";
+            // Build the body of your email using the Body property of your message
+            msg.Body = message;
+
+            // Wires up and send the email
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = loginInfo;
+            smtpClient.Send(msg);
         }
-        */
 
         public ActionResult Nutzungsbedingungen()
         {
