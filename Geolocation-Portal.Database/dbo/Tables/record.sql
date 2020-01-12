ba2b41f94ff37dd907e319ea350ad67f38806032
Starhammer <manuelscheub@gmail.com>
2020-01-12 16:41:31 +0100

    Location in DB eingefügt

--- Geolocation-Portal.Database/dbo/Tables/record.sql
+++ Geolocation-Portal.Database/dbo/Tables/record.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[record]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
     [dataset_upload] DATETIME2 NOT NULL, 
     [dataset_modified_date] DATETIME2 NOT NULL,
 	 [title] NVARCHAR(50) NOT NULL, 
@@ -11,7 +11,9 @@
 	 [rating] INT NULL,
 	 [role_id] INT NULL,
 	 [record_active] BIT NOT NULL DEFAULT 0, 
+    [location_id] INT NOT NULL, 
     CONSTRAINT [FK_record_category] FOREIGN KEY (category_id) REFERENCES category(id), 
-    CONSTRAINT [FK_record_publisher] FOREIGN KEY (publisher_id) REFERENCES publisher(id)
+    CONSTRAINT [FK_record_publisher] FOREIGN KEY (publisher_id) REFERENCES publisher(id), 
+    CONSTRAINT [FK_record_location] FOREIGN KEY (location_id) REFERENCES location(id)
 
 )
