b889a4599bbdbfd91e51f5b13eefd92f007d1bc2
Micha <michael.kress92@web.de>
2020-01-26 10:53:22 +0100

    Kommentieren der Index unter OpenData und GeoData; Fehlerbehebungen --> Funktion Zeige auch die nicht aktivierten Datensätze (Icon wird nun korrekt getauscht); Bearbeiten der "addMapRecordToShoppingCart" Funktion.

--- Geolocation-Portal_Test/Controllers/GeoDataController.cs
+++ Geolocation-Portal_Test/Controllers/GeoDataController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,12 +9,33 @@ namespace Geolocation_Portal_Test.Controllers
 {
     public class GeoDataController : Controller
     {
-        // GET: GeoData
+        /// <summary>
+        /// This action result returns the kategoriebearbeitung view. Checks whether one or more data sets should be displayed on the map.
+        /// </summary>
+        /// <param name="id">An ID is required to load the geojson file from a record.</param>
+        /// <returns>Returns a view to the browser.</returns>
         public ActionResult Index(int? id)
         {
             if (id != null)
             {
-                ViewBag.FileId = id;
+                // ToDo: Session in Json Variable speichern.
+                //Json recordList = Session["MapRecordShoppingCart"];
+
+                //Session["MapRecordShoppingCart"] = "";
+
+                if (Session["MapRecordShoppingCart"] != null)
+                {
+                    // Session Variable an View übergeben.
+                    int[] recordIDs = { 4003, 5004, 7002 };
+
+                    ViewBag.FileId = recordIDs;
+                }
+                else
+                {
+                    int[] recordIDs = { Convert.ToInt32(id, new CultureInfo("de-DE")) };
+
+                    ViewBag.FileId = recordIDs;
+                }
             }
             
             return View();
